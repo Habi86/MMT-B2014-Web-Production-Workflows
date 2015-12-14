5e21f1b384022c348b06413e46d3c670738ab632
Stefanie Habersatter <shabersatter.mmt-b2014@fh-salzburg.ac.at>
2015-12-14 13:57:35 +0100

    Eliminate this_amount by using Replace Temp with Query

--- flo-movie-rental.git/customer.rb
+++ flo-movie-rental.git/customer.rb
@@ -14,7 +14,6 @@ class Customer
     total_amount, frequent_renter_points = 0, 0
     result = "Rental Record for #{@name}\n"
     @rentals.each do |element|
-      this_amount = amount_for(element)
 
       # add frequent renter points
       frequent_renter_points += 1
@@ -22,18 +21,15 @@ class Customer
       if element.movie.price_code == Movie::NEW_RELEASE && element.days_rented > 1
         frequent_renter_points += 1
       end
+
       # show figures for this rental
-      result += "\t" + element.movie.title + "\t" + this_amount.to_s + "\n"
-      total_amount += this_amount
+      result += "\t" + element.movie.title + "\t" + element.charge.to_s + "\n"
+      total_amount += element.charge
     end
+
     # add footer lines
     result += "Amount owed is #{total_amount}\n"
     result += "You earned #{frequent_renter_points} frequent renter points"
     result
-  end
-
-  #changed amount_for so it works in rental.rb
-  def amount_for(rental)
-    rental.charge
   end
 end
